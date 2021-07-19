a1d649fd94c8da900d0476e7c187ff5d0823d0cd
Guilherme Soster <GSoster@users.noreply.github.com>
2021-07-19 11:29:13 -0300

    Update solution/project structure Add unit test project

---
+++ rpg_combat/rpg_combat.test/Controllers/CharacterControllerTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rpg_combat.Controllers;
+
+namespace rpg_combat.test
+{
+    [TestClass]
+    public class CharacterControllerTest
+    {
+        private CharacterController controller;
+        
+        [TestMethod]
+        public void GetAllCharactersShouldReturnList()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
